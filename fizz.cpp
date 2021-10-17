798e68c6b62745df2cfb4d6e0830fd0a0b09a358
KlevaBot <65098363+KlevaBot@users.noreply.github.com>
2021-10-17 07:58:47 +0200

    Update fizz.cpp

--- fizz.cpp
+++ fizz.cpp
@@ -1,17 +1,17 @@
 
 #include <iostream>
-#include <cassert>
+#include <cassert>  //needed by assert
 using namespace std;
 
-
+//used a function, you could also just do the whole program in main
 void printit(int n)
 {
     for(int i =1;i<=n;i++)
-    {    assert(!n>200);
+    {    assert(!n>200); // just to avoid higher  values, you can remove it 
         if(i%15==0)
             cout<<"fizzbuzzzz"<<endl;
         else if (i%5==0)
-            cout<<"buzz"<<endl;
+            cout<<"buzz"<<endl;     
         else if(i%3==0)
             cout<<"fizz"<<endl;
         else
@@ -20,9 +20,9 @@ void printit(int n)
 }
 
 int main()
-{ int n;
+{ int num;
 cout<<"We are going to do the fizzbuzz test, \
-now enter an integer from 1-100 : ";cin>>n;
-   printit(n);
+now enter an integer from 1-100 : ";cin>>num;
+   printit(num);  //calling of the functiom\n 
 return 0;
 }
